156e893a59dd49ff37547a504d64b7f09d2d5be1
AnaghaChikalthankar <87253774+AnaghaChikalthankar@users.noreply.github.com>
2021-07-19 21:55:52 +0530

    Add files via upload

---
+++ Exception/Assignment3/TestAgeCalculator1.java
@@ -0,0 +1,18 @@
+package Assignment3_Exception;
+import java.util.Scanner;
+public class TestAgeCalculator1 {
+		public static void main(String[] args) {
+
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter DOB");
+			String str = sc.nextLine();
+			AgeCalculator1 age = new AgeCalculator1();
+						
+			try {
+				int a = age.calculateAge(str);
+				System.out.println(a);
+			}catch(Exception e) {
+				System.out.println("Date of birth should be in dd/mm/yyyy format");
+			}}}
+
+
