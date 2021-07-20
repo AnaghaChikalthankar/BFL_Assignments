6ce0b9abacc677c8cb0114a11e313cdcb5a57b9e
AnaghaChikalthankar <87253774+AnaghaChikalthankar@users.noreply.github.com>
2021-07-20 08:56:51 +0530

    Add files via upload

---
+++ File Handling/Assignment7/Engine.java
@@ -0,0 +1,31 @@
+package File_Handling;
+
+import java.io.Serializable;
+
+public class Engine implements Serializable {
+	private String engineNo;
+	private int cubicCapacity;
+	
+	public Engine(String engineNo, int cubicCapacity) {
+		super();
+		this.engineNo = engineNo;
+		this.cubicCapacity = cubicCapacity;
+	}
+	
+	public String getEngineNo() {
+		return engineNo;
+	}
+	public void setEngineNo(String engineNo) {
+		this.engineNo = engineNo;
+	}
+	public int getCubicCapacity() {
+		return cubicCapacity;
+	}
+	public void setCubicCapacity(int cubicCapacity) {
+		this.cubicCapacity = cubicCapacity;
+	}
+	
+	
+	
+
+}
